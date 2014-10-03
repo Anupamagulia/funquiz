097c04d82b964493fc53feb6c6de7b2f84e88da9
Anupamagulia <guliaanupama34@gmail.com>
2014-10-03 21:42:47 +0500

    Funquiz
    This is a Fun filled quiz game.In this game we have three categories of
    quiz for the users and these are :-
    1) Guess the images.
    2) Guess the Songs.
    3) Guess the Movie Trailers.
    
    All the Quiz Questions come randomly in particular category.

---
+++ QuizPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Coding4Fun.Toolkit.Controls;
+
+
+using System.Net;
+using System.Windows;
+
+using Windows.UI.Xaml.Documents;
+using System.Windows.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Shapes;
+
+using Windows.UI.Xaml.Media.Imaging;
+using System.ComponentModel;
+using System.IO;
+
+using Windows.UI.Xaml.Resources;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace funquiz
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class QuizPage : Page
+    {
+        private SoundEffect correctSound, wrongSound;
+        public QuizPage()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.  The Parameter
+        /// property is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+    }
+}
